7da6efc4d71b8e7befca24b16a8c7751dc069c8c
aa055 <aamirmoiz531@outlook.com>
2025-07-08 00:04:35 +0400

    Updated the webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,12 @@
-const Dotenv = require('dotenv-webpack');
-const fs = require('fs');
-
-const dotenvPath = './.env';
-const dotenvExists = fs.existsSync(dotenvPath);
+const webpack = require('webpack');
 
 module.exports = {
   plugins: [
-    new Dotenv({
-      path: dotenvExists ? dotenvPath : undefined,
-      silent: true // suppress warnings if .env missing
+    new webpack.DefinePlugin({
+      'process.env': {
+        SUPABASE_URL: JSON.stringify(process.env.SUPABASE_URL),
+        SUPABASE_KEY: JSON.stringify(process.env.SUPABASE_KEY)
+      }
     })
   ]
 };
