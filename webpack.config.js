7f40eadd5912afa04e7e01c3f7de3aba57c8d383
aa055 <aamirmoiz531@outlook.com>
2025-07-07 23:50:51 +0400

    Update the webpack config file

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,14 @@
 const Dotenv = require('dotenv-webpack');
+const fs = require('fs');
+
+const dotenvPath = './.env';
+const dotenvExists = fs.existsSync(dotenvPath);
+
 module.exports = {
   plugins: [
-    new Dotenv()
+    new Dotenv({
+      path: dotenvExists ? dotenvPath : undefined,
+      silent: true // suppress warnings if .env missing
+    })
   ]
 };
