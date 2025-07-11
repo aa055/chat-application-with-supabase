514fbf4b3ddee87ac13468b0720ed9969a1d2024
aa055 <aamirmoiz531@outlook.com>
2025-07-11 21:45:12 +0400

    Removed the environment file references

--- src/app/supabase/chat.service.ts
+++ src/app/supabase/chat.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, signal } from '@angular/core';
 import { createClient, SupabaseClient } from '@supabase/supabase-js';
-import { environment } from '../../environments/environment.development';
+// import { environment } from '../../environments/environment.development';
 import { Ichat } from '../interface/chat-response';
 
 @Injectable({
